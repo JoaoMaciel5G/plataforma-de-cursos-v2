f38988011da683de569cc8a8226c641678de49a5
JoaoMaciel5G <juauvitu765@gmail.com>
2024-03-22 14:25:48 -0300

    feat: add types interface courses and plains

---
+++ src/app/_types/plainsInterface.ts
@@ -0,0 +1,8 @@
+export interface PlainSignatureProps {
+    price: string;
+    discountPrince: string;
+    id: string;
+    description: string;
+    option: string;
+    imageUrl: string | null;
+}
